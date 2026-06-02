1c46e730891c12b6dd125070fc02528b5d88072b
fahim2023 <fa733m4d4m@outlook.com>
2026-06-02 20:58:16 +0100

    Configure DEBUG setting for production

--- forkit/settings.py
+++ forkit/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = ["forkit-app-397f94f54229.herokuapp.com", "127.0.0.1"]
 # Application definition
 
