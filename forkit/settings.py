2de1ada9de01f6d1c1e14ee1d38df3e97e4ac502
fahim2023 <fa733m4d4m@outlook.com>
2026-06-02 20:39:05 +0100

    Configure PostgreSQL database and run initial migrations

--- forkit/settings.py
+++ forkit/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "recipies",
+    "recipes",
 ]
 
 MIDDLEWARE = [
@@ -79,12 +79,7 @@ WSGI_APPLICATION = "forkit.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 
 # Password validation
