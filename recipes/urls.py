e451883bca8172353121e003798eeb5c743af7e4
fahim2023 <fa733m4d4m@outlook.com>
2026-06-04 23:04:06 +0100

    Add URL pattern for recipe edit view

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("recipe/create/", views.recipe_create, name="recipe_create"),
     path("recipe/<slug:slug>/", views.recipe_detail, name="recipe_detail"),
+    path("recipe/<slug:slug>/edit/", views.recipe_edit, name="recipe_edit"),
 ]
