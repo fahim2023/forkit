dcc4cbbdc61925a877bc16e5b4e277be8ad6f64f
fahim2023 <fa733m4d4m@outlook.com>
2026-06-04 11:53:45 +0100

    Add home view to display all recipes and categories

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+from django.db.models import Avg, Q
+from .models import Recipe, Category
+
 # Create your views here.
+
+
+def home(request):
+    """
+    Display the homepage with all recipes.
+    Supports search by keyword and filtering by category.
+    """
+    recipes = Recipe.objects.all().annotate(avg_rating=Avg("ratings__score"))
+    categories = Category.objects.all()
+
+    context = {
+        "recipes": recipes,
+        "categories": categories,
+    }
+    return render(request, "recipes/home.html", context)
