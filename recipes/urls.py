5bdc78e1d608a94dc6164d5ae6a6fc73ff305b80
fahim2023 <fa733m4d4m@outlook.com>
2026-06-04 17:39:11 +0100

    Add URL pattern for recipe create view

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("recipe/<slug:slug>/", views.recipe_detail, name="recipe_detail"),
+    path("recipe/create/", views.recipe_create, name="recipe_create"),
 ]
