049b8c73cdb9e81cc7f074866686260b9e4e6d8b
fahim2023 <fa733m4d4m@outlook.com>
2026-06-06 10:51:47 +0100

    Add URL pattern for comment delete view

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("recipe/<slug:slug>/", views.recipe_detail, name="recipe_detail"),
     path("recipe/<slug:slug>/edit/", views.recipe_edit, name="recipe_edit"),
     path("recipe/<slug:slug>/delete/", views.recipe_delete, name="recipe_delete"),
+    path(
+        "comment/<int:comment_id>/delete/", views.comment_delete, name="comment_delete"
+    ),
 ]
