ca5cad7d588a92ae5090e4e2ad9eede613322b2d
fahim2023 <fa733m4d4m@outlook.com>
2026-06-06 20:42:21 +0100

    Add custom 404 page with redirect to homepage

--- recipes/views.py
+++ recipes/views.py
@@ -180,3 +180,8 @@ def profile(request):
 
     context = {"recipes": recipes}
     return render(request, "recipes/profile.html", context)
+
+
+def custom_404(request, exception):
+    """Custom 404 error page."""
+    return render(request, "404.html", status=404)
